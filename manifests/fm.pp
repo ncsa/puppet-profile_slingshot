a8dcbb4e09abdb32143435a0997bfc75684d5505
David King <kingda@illinois.edu>
2022-11-22 11:52:13 -0600

    set nginx version

--- manifests/fm.pp
+++ manifests/fm.pp
@@ -7,9 +7,14 @@
 class profile_slingshot::fm (
   Boolean         $enable,
   Array[ String ] $required_pkgs,
+  String          $nginxversion
 ) {
 
   if ($enable) {
+    package( 'nginx'
+      enable_only => true
+      ensure      => $nginxversion
+    )
     $packages_defaults = {
     }
     ensure_packages( $required_pkgs, $packages_defaults )
