2659a55862352e34b27b78272846a945cb3356e1
David King <kingda@illinois.edu>
2022-11-22 12:23:02 -0600

    try reorder and fix

--- manifests/fm.pp
+++ manifests/fm.pp
@@ -15,17 +15,18 @@ class profile_slingshot::fm (
     #  ensure      => 'nginx:1.16',
     #  enable_only => 'true',
     #}
-    exec { 'dnf-enable':
-      path        =>  $path,
-      command     =>  'dnf config-manger --enable nginx:1.16 container-tools',
-      refreshonly =>  true,
-    }
     exec { 'dnf-modules':
       path        =>  $path,
       command     =>  'dnf -y module reset container-tools nginx',
       require     =>  Exec['dnf-enable'],
       refreshonly => true,
     }
+    exec { 'dnf-enable':
+      path        =>  $path,
+      command     =>  'dnf -y module enable nginx:1.16 container-tools',
+      refreshonly =>  true,
+    }
+
     $packages_defaults = {
     }
     ensure_packages( $required_pkgs, $packages_defaults )
