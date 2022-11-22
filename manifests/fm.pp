ae2bf21a3002e0116747c3cbddde13b64f27312c
David King <kingda@illinois.edu>
2022-11-22 12:52:01 -0600

    attempt order

--- manifests/fm.pp
+++ manifests/fm.pp
@@ -11,18 +11,20 @@ class profile_slingshot::fm (
 
   if ($enable) {
     exec { 'dnf-modules':
-      path        =>  $path,
-      command     =>  'dnf -y module reset container-tools nginx',
-      require     =>  Exec['dnf-enable'],
+      path        => $path,
+      command     => 'dnf -y module reset container-tools nginx',
+      before      => Exec['dnf-enable'],
       refreshonly => true,
     }
     exec { 'dnf-enable':
-      path        =>  $path,
-      command     =>  'dnf -y module enable nginx:1.16 container-tools',
-      refreshonly =>  true,
+      path        => $path,
+      command     => 'dnf -y module enable nginx:1.16 container-tools',
+      refreshonly => true,
+      before      => Package['slingshot-fmn-redhat']
     }
     package { 'slingshot-fmn-redhat':
-      ensure      => installed,
+      ensure  => installed,
+      require => 
     }
   }
 }
